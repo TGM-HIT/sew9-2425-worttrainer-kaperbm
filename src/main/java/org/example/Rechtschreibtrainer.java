e7258e833d819ae505491a9d73feda8b336e92c4
kacper <kaperbm11@gmail.com>
2024-09-18 16:43:34 +0200

    Hinzufügen des Model-UML und hinzufügen der Klassenstruktur

---
+++ src/main/java/org/example/Rechtschreibtrainer.java
@@ -0,0 +1,27 @@
+public class Rechtschreibtrainer {
+
+	private int gesamteVersuche;
+
+	private int richitg;
+
+	private int falsch;
+
+	private List<TrainingsDaten> TrainingsDatenListe;
+
+	private int aktuellerIndex;
+
+	private TrainingsDaten trainingsDaten;
+
+	public void selectPaar(int index) {
+
+	}
+
+	public void selectRandomPaar() {
+
+	}
+
+	public String getStatistic() {
+		return null;
+	}
+
+}
---
+++ src/main/java/org/example/TrainingsDaten.java
@@ -0,0 +1,23 @@
+public class TrainingsDaten {
+
+	private String wort;
+
+	private String url;
+
+	public TrainingsDaten(String wort, String url) {
+
+	}
+
+	public String getUrl() {
+		return null;
+	}
+
+	public String getWort() {
+		return null;
+	}
+
+	public boolean checkValid() {
+		return false;
+	}
+
+}
